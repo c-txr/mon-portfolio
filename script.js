f9f630ba40808dab493fbe6f572b47d5ff3786a5
c-txr <chaden.teixeira78@gmail.com>
2026-01-12 15:23:12 +0100

    fix bugs

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /* Faire en sorte que la lueur de lumière de la section hero suive les mouvements de la souris*/
 
+/* Je stock ma section où se trouve mon cercle lumineux dans une variable*/
 const background = document.querySelector(".hero")
 
 background.addEventListener("mousemove", function(lueur){
